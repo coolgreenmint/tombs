40dd096ae35bff09758650c9dfc32c60569fd04e
mint <mint@mint-thinkpad>
2020-10-24 16:43:42 +0800

    项目模板创建

---
+++ tombs/src/main/java/com/mint/tombs/TombsApplication.java
@@ -0,0 +1,13 @@
+package com.mint.tombs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TombsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TombsApplication.class, args);
+	}
+
+}
---
+++ tombs/src/test/java/com/mint/tombs/TombsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mint.tombs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TombsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
