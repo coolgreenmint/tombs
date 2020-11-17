72163535f12f38b518b51740a2d9c483c93af12d
mint <sanguolian_it@163.com>
2020-11-17 23:21:35 +0800

    二分法查找数据

--- tombs/src/main/java/com/mint/tombs/algorithm/Bin_Search.java
+++ tombs/src/main/java/com/mint/tombs/algorithm/Bin_Search.java
@@ -1,5 +1,6 @@
 package com.mint.tombs.algorithm;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -17,29 +18,77 @@ import java.util.List;
 public class Bin_Search {
 	/**
 	 * 
-	     * <p>Title: comm_search</p>  
-	     * <p>Description: 普通方式查找  即循环遍历的方式</p>
+	 * <p>
+	 * Title: comm_search
+	 * </p>
+	 * <p>
+	 * Description: 普通方式查找 即循环遍历的方式
+	 * </p>
 	 */
-	public static void comm_search(List<Integer> list,int num) {
+	public static void comm_search(List<Integer> list, int num) {
+		long start = System.currentTimeMillis();
 		boolean isFind = false;
 		for (int i = 0; i < list.size(); i++) {
 			if (list.get(i) == num) {
 				isFind = true;
-			    System.out.println("第" + i + "次查找，当前数字为：" + list.get(i) + "， 查找数字为：" + num );
+				System.out.println("第" + i + "次查找，当前数字为：" + list.get(i) + "， 查找数字为：" + num);
 				break;
+			} else {
+				System.out.println("第" + i + "次查找，当前数字为：" + list.get(i) + "， 查找数字为：" + num);
 			}
 		}
-		
+
 		if (isFind) {
 			System.out.println("找到了猜想数字！");
-		}
-		else {
+		} else {
 			System.out.println("根本就没有你说的数字！");
 		}
-	}
-	
-	public static void main(String[] args) {
-		
+		System.out.println("方法运行结束，共花费" + (System.currentTimeMillis() - start) + "毫秒");
 	}
 
+	/**
+	 * 
+	 * <p>
+	 * Title: bin_search
+	 * </p>
+	 * <p>
+	 * Description: 利用二分查找法查找特定数字
+	 * </p>
+	 * 
+	 * @param list 查找的有序集合
+	 * @param num  需要查找的数字
+	 */
+	public static void bin_search(List<Integer> list, int num) {
+		long start = System.currentTimeMillis();
+		int low = 1;
+		int height = list.size() - 1;
+
+		while (low <= height) { // 只要最小值小于或等于最大值，就进行猜测
+			int mid = (low + height) / 2;
+			int guess = list.get(mid);
+
+			if (guess == num) {
+				System.out.println("找到了你說的數字，他是" + guess);
+				break;
+			} else if (guess < num) {
+				System.out.println("小啦！");
+				low = mid + 1;
+			} else {
+				System.out.println("大啦！");
+				height = mid - 1;
+			}
+		}
+		System.out.println("方法运行结束，共花费" + (System.currentTimeMillis() - start) + "毫秒");
+	}
+
+	public static void main(String[] args) {
+
+		List<Integer> lstNums = new ArrayList<Integer>();
+		for (int i = 1; i < 500000; i++) {
+			lstNums.add(i);
+		}
+
+		comm_search(lstNums, 99999);
+		bin_search(lstNums, 99999);
+	}
 }
